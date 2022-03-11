d2e52cbbeb3043ea145e4b9ddd0b66c1be8c5b5b
Amartuvshin <96567743+amrashk@users.noreply.github.com>
2022-03-11 15:33:51 +0800

    Add files via upload

---
+++ 4.3/java/mn/edu/num/droidcafeoptions/FirstFragment.java
@@ -0,0 +1,41 @@
+package mn.edu.num.droidcafeoptions;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import mn.edu.num.droidcafeoptions.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
